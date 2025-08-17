2bfad34a6dd119738e01757326e13ab0780e5e90
Tamim Arafat <31634638+arafatamim@users.noreply.github.com>
2025-08-17 09:58:46 +0600

    Refactor useSave to simplify state and API calls

--- inertia/composables/use_save.ts
+++ inertia/composables/use_save.ts
@@ -1,41 +1,29 @@
 import { router } from "@inertiajs/vue3";
 import { ref } from "vue";
-import type { PostMeta } from "~/types/post_meta";
 
-export default function useSave(
-  postId: number,
-  initialValue: PostMeta = {
-    isSaved: false,
-    savesCount: 0,
-  },
-) {
-  const isSaved = ref(initialValue.isSaved);
-  const savesCount = ref(initialValue.savesCount);
+export default function useSave(postId: number, isSaved: boolean) {
+  const isSavedRef = ref(isSaved);
 
   const toggleSave = () => {
-    if (isSaved.value) {
-      // Unlike the post
+    if (isSavedRef.value) {
       router.delete(`/posts/${postId}/save`, {
         preserveState: true,
         preserveScroll: true,
-        onSuccess: (page) => {
-          isSaved.value = false;
-          savesCount.value = Math.max(0, savesCount.value - 1);
-          router.reload({ only: ["savedPosts"] });
+        only: ["savesCount", "isSaved"],
+        onSuccess: () => {
+          isSavedRef.value = false;
         },
       });
     } else {
-      // Like the post
       router.post(
         `/posts/${postId}/save`,
         {},
         {
           preserveState: true,
           preserveScroll: true,
-          onSuccess: (page) => {
-            isSaved.value = true;
-            savesCount.value += 1;
-            router.reload({ only: ["savedPosts"] });
+          only: ["savesCount", "isSaved"],
+          onSuccess: () => {
+            isSavedRef.value = true;
           },
         },
       );
@@ -43,8 +31,7 @@ export default function useSave(
   };
 
   return {
-    isSaved,
-    savesCount,
+    isSaved: isSavedRef,
     toggleSave,
   };
 }
