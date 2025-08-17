16b58fda85fef8617ee3e8e0b9ab3e61a1099eb2
Tamim Arafat <31634638+arafatamim@users.noreply.github.com>
2025-08-17 22:19:10 +0600

    Use Inertia location redirects in login and signup

--- app/controllers/auth_controller.ts
+++ app/controllers/auth_controller.ts
@@ -3,23 +3,23 @@ import { loginValidator, signupValidator } from "#validators/auth";
 import type { HttpContext } from "@adonisjs/core/http";
 
 export default class AuthController {
-  async login({ request, response, auth }: HttpContext) {
+  async login({ request, auth, inertia }: HttpContext) {
     const { username, password, rememberMe } =
       await request.validateUsing(loginValidator);
     const user = await User.verifyCredentials(username, password);
 
     await auth.use("web").login(user, rememberMe);
 
-    response.redirect(`/${user.username}`);
+    return inertia.location(`${user.username}`);
   }
 
-  public async signup({ request, response, auth }: HttpContext) {
+  public async signup({ request, inertia, auth }: HttpContext) {
     const { username, password } = await request.validateUsing(signupValidator);
     const user = await User.create({ username, password });
 
     await auth.use("web").login(user);
 
-    response.redirect("/settings");
+    return inertia.location("/settings");
   }
 
   public async logout({ response, auth }: HttpContext) {
